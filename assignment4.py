8d77a88cfe747aa030a3f3a9fe1d1bc9344c20ff
PoojaB29 <58439134+PoojaB29@users.noreply.github.com>
2020-10-10 20:09:55 +0530

    Add files via upload
    Dictionary manipulation.

---
+++ assignment4.py
@@ -0,0 +1,23 @@
+def mystery(l,v):
+  if len(l) == 0:
+    return (v)
+  else:
+    return (mystery(l[:-1],l[-1]+v))
+a=mystery([22,14,19,65,82,55],1)
+print(a)
+print(type(a))
+
+triples = [ (x,y,z) for x in range(2,4) for y in range(2,5) for z in range(5,7) if 2*x*y > 3*z ]
+print(triples)
+
+runs = {"Test":{"Rahul":[90,14,35],"Kohli":[3,103,73,42],"Pujara":[53,15,133,8]},"ODI":{"Sharma":[37,99],"Kohli":[63,47]}}
+#runs["ODI"]["Rahul"].append([74])
+#runs["ODI"]["Rahul"].extend([74])
+#runs["ODI"]["Rahul"][0]=74
+runs["ODI"]["Rahul"]=[74]
+
+actor={}
+actor["Star Wars"] = ["Rey","Ridley"]
+actor["Star Wars, Rey"] = "Ridley"
+#actor[["Star Wars", "Rey"]] = "Ridley"
+actor[("Star Wars", "Rey")] = "Ridley"
