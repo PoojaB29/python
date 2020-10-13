6c160594a4771b531006ec538bc886f593c699fe
PoojaB29 <58439134+PoojaB29@users.noreply.github.com>
2020-10-13 16:37:41 +0530

    Dictionary implementation.

--- assignment4.py
+++ assignment4.py
@@ -1,3 +1,6 @@
+#Created by Pooja
+#On 22 Feb 2020
+#The program explains the implementation of dictionary in python.
 def mystery(l,v):
   if len(l) == 0:
     return (v)
@@ -11,13 +14,13 @@ triples = [ (x,y,z) for x in range(2,4) for y in range(2,5) for z in range(5,7) 
 print(triples)
 
 runs = {"Test":{"Rahul":[90,14,35],"Kohli":[3,103,73,42],"Pujara":[53,15,133,8]},"ODI":{"Sharma":[37,99],"Kohli":[63,47]}}
-#runs["ODI"]["Rahul"].append([74])
-#runs["ODI"]["Rahul"].extend([74])
-#runs["ODI"]["Rahul"][0]=74
+runs["ODI"]["Rahul"].append([74])
+runs["ODI"]["Rahul"].extend([74])
+disruns["ODI"]["Rahul"][0]=74
 runs["ODI"]["Rahul"]=[74]
 
 actor={}
 actor["Star Wars"] = ["Rey","Ridley"]
 actor["Star Wars, Rey"] = "Ridley"
-#actor[["Star Wars", "Rey"]] = "Ridley"
+actor[["Star Wars", "Rey"]] = "Ridley"
 actor[("Star Wars", "Rey")] = "Ridley"
