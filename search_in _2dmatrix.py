e5a8729e15ab7877ff6c489d51c61318aa54756a
PoojaB29 <58439134+PoojaB29@users.noreply.github.com>
2020-10-16 20:44:45 +0530

    Implemented search element in 2D matrix.

---
+++ search_in _2dmatrix.py
@@ -0,0 +1,21 @@
+# Search element in 2dmatrix and martix contain sorted element
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+        height = len(matrix)
+        width = len(matrix[0])
+        
+        row = height -1
+        col = 0
+        
+        while col < width and row >=0:
+            if matrix[row][col] > target:
+                row = row -1
+            elif matrix[row][col] < target:
+                col = col + 1
+            else :
+                return True
+        return False
+            
+            
